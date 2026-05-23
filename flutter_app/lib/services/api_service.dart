43854e6e76dc3977f8cb2f35fb92dde98cc58138
yazeed-zz <yazeedahmedyassin@gmail.com>
2026-05-23 03:20:57 +0300

    Update api_service.dart

--- flutter_app/lib/services/api_service.dart
+++ flutter_app/lib/services/api_service.dart
@@ -3,7 +3,7 @@ import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
 
 class ApiService {
-  static const String baseUrl = 'http://192.168.0.199:8080/api';
+  static const String baseUrl = 'http://192.168.0.155:8080/api';
 
   static Future<String?> getToken() async {
     final prefs = await SharedPreferences.getInstance();
